9baa895e94484e5df25c1f1ead637d3d26e72202
supriyag <supriyag@VMLAP134.VIVID.AD>
2026-09-04 17:17:21 +0530

    new changes

---
+++ test1/src/test/java/test1/login.java
@@ -0,0 +1,24 @@
+package test1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class login {
+	@Test
+	public void Login() throws InterruptedException 
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		//To maximize the window
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.get("https://www.google.com/");
+		Thread.sleep(2000);
+		driver.findElement(By.id("APjFqb")).sendKeys("Amazon");
+	}
+
+}
